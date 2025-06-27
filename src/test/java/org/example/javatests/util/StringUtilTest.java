cc701459c871392d56c945c83bbbe6da09a86239
jesusbeb <jbeltranb@outlook.com>
2025-06-26 18:34:40 -0600

    Creacion de Tests Unitarios y Excepciones

--- src/test/java/org/example/javatests/util/StringUtilTest.java
+++ src/test/java/org/example/javatests/util/StringUtilTest.java
@@ -3,19 +3,23 @@ package org.example.javatests.util;
 public class StringUtilTest {
     public static void main(String[] args) {
 
-        //Hacemos unas pequeñas pruebas basicas al metodo repeat
         String result = StringUtil.repeat("Hola", 3);
-        System.out.println(result);
-        if (result.equals("HolaHolaHola")){
-            System.out.println("OK");
+        assertEquals(result, "holaholahola");
+
+        //Simplificamos el codigo Inline. Clic en menu Refactor, Inline Variable
+        //asi evitamos tener la variable result
+        assertEquals(StringUtil.repeat("Hola", 1),"hola");
+    }
+
+
+    //Escribimos un metodo para comparar el resultado obtenido y el esperado
+    //Lanzamos una excepcion en casa de haber un error y nos indicara la linea donde
+    //se provoco el error.
+    //Un test unitario es para probar una parte especifica del programa, en este caso
+    //testeamos un metodo
+    private static void assertEquals(String actual, String expected) {
+        if (!actual.equals(expected)){
+            throw new RuntimeException(actual +" no es igual a expected: " +expected);
         }
-
-        String result2 = StringUtil.repeat("Hola", 1);
-        System.out.println(result2);
-        if (!result2.equals("hola")){
-            System.out.println("ERROR");
-        }
-
-
     }
 }
