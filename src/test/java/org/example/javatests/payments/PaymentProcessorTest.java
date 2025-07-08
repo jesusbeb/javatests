396087be3db481a194e119437373d3f31afbd856
jesusbeb <jbeltranb@outlook.com>
2025-07-08 10:10:52 -0600

    Organizacion y Mejora de Tests en jUnit

--- src/test/java/org/example/javatests/payments/PaymentProcessorTest.java
+++ src/test/java/org/example/javatests/payments/PaymentProcessorTest.java
@@ -1,5 +1,6 @@
 package org.example.javatests.payments;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.mockito.Mockito;
 
@@ -7,18 +8,27 @@ import static org.junit.Assert.*;
 
 public class PaymentProcessorTest {
 
+    private PaymentGateway paymentGateway;
+    private PaymentProcessor paymentProcessor;
+
+    //Creamos este metodo para agregar las lineas de codigo repetidas en los Test, para tener
+    //menos codigo. Este metodo se suele llamar setup() y se usa la anotacion @Before para
+    //indicar que se tiene que ejecutar siempre antes que cualquier Test
+    @Before
+    public void setup(){
+        //Como el paymentGateway no lo tenemos, lo simulamos con Mockito
+        paymentGateway = Mockito.mock(PaymentGateway.class);
+        //Creamos el paymentProcessor que recibe como parametro un paymenGateway
+        paymentProcessor = new PaymentProcessor(paymentGateway);
+    }
+
     @Test
     public void payment_is_correct(){
 
-        //Como el paymentGateway no lo tenemos, lo simulamos con Mockito
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         //Cuando al paymentGateway se le haga un requestPayment cualquiera, entonces
         //retorne un PaymentStatus de OK
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.OK));
-
-        //Creamos el paymentProcessor que recibe como parametro un paymenGateway
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         //Comprobamos que cuando el paymentProcessor intenta hacer el pago de X cantidad, sera correcto
         assertTrue(paymentProcessor.makePayment(1000));
@@ -27,11 +37,8 @@ public class PaymentProcessorTest {
     @Test
     public void payment_is_wrong(){
 
-        PaymentGateway paymentGateway = Mockito.mock(PaymentGateway.class);
         Mockito.when(paymentGateway.requestPayment(Mockito.any()))
                 .thenReturn(new PaymentResponse(PaymentResponse.PaymentStatus.ERROR));
-
-        PaymentProcessor paymentProcessor = new PaymentProcessor(paymentGateway);
 
         assertFalse(paymentProcessor.makePayment(1000));
     }
